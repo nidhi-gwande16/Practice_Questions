07c12d6a186598c3f4b5d10265dda3359248bcbe
nidhi-gwande16 <133477564+nidhi-gwande16@users.noreply.github.com>
2024-12-22 18:28:37 +0530

    Time: 3 ms (62.93%), Space: 42.8 MB (14.41%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.java
@@ -0,0 +1,62 @@
+/*class Solution {
+    public boolean buddyStrings(String s, String goal) {
+        if(s.length() != goal.length())
+            return false;
+        if(s.equals(goal))
+        {
+            Set<Character> set=new HashSet();
+            for(char ch:s.toCharArray())
+            {
+                if(set.contains(ch))
+                return true;
+            }
+            return false;
+        }
+        else{
+            List<Integer> l=new ArrayList();
+            for(int i=0;i<s.length();i++)
+            {
+                if(s.charAt(i) !=goal.charAt(i))
+                {
+                    return l.add(i);
+                }
+            }
+            return l.size() == 2 
+                && s.charAt(l.get(0))==goal.charAt(l.get(1)) 
+                && s.charAt(l.get(1))==goal.charAt(l.get(0));
+        }
+    }
+}
+*/
+class Solution {
+    public boolean buddyStrings(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        if (s.equals(goal)) {
+            // Check if there is at least one duplicate character in the string
+            Set<Character> set = new HashSet<>();
+            for (char ch : s.toCharArray()) {
+                if (set.contains(ch)) {
+                    return true;
+                }
+                set.add(ch);
+            }
+            return false;
+        } else {
+            // Find the indices where characters differ
+            List<Integer> diff = new ArrayList<>();
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) != goal.charAt(i)) {
+                    diff.add(i);
+                }
+            }
+
+            // There should be exactly two differences, and swapping them should make the strings equal
+            return diff.size() == 2 &&
+                   s.charAt(diff.get(0)) == goal.charAt(diff.get(1)) &&
+                   s.charAt(diff.get(1)) == goal.charAt(diff.get(0));
+        }
+    }
+}
