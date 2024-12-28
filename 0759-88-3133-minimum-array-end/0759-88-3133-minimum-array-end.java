da1ad454048417f321a09f462ed4a14e8c9bce71
nidhi-gwande16 <133477564+nidhi-gwande16@users.noreply.github.com>
2024-12-28 18:25:52 +0530

    Time: 1744 ms, Memory: 40.9 MB - LeetHub

---
+++ 0759-88-3133-minimum-array-end/0759-88-3133-minimum-array-end.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long minEnd(int n, int x) {
+        long ans=x;
+        n=n-1;
+        while(n-->0)
+        {
+            ans=(ans+1) |x;
+        }
+        return ans;
+
+    }
+}
